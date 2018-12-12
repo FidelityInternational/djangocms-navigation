d63b979a351b57cabe7b7a1a57c7943212e663d7
Rizwan <rizwan@webbyfox.com>
2018-12-12 14:13:45 +0000

    Skeleton app (#1)

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+import djangocms_navigation
+
+
+INSTALL_REQUIREMENTS = [
+    'Django>=1.11,<2.0',
+    'django-cms>=3.5.0'
+    ]
+
+
+setup(
+    name='djangocms-navigation',
+    packages=find_packages(),
+    include_package_data=True,
+    version=djangocms_navigation.__version__,
+    description=djangocms_navigation.__doc__,
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+    install_requires=INSTALL_REQUIREMENTS,
+    author='Fidelity International',
+    url='https://github.com/fidelityInternational/djangocms-navigation',
+    license='BSD',
+    test_suite='tests.settings.run',
+)
