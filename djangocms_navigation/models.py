59bb892b2d8d66fc8509cc1a646617fb8fa83546
Krzysztof Socha <ksocha@ksocha.com>
2018-12-17 12:17:23 +0100

    Add navigation plugin (#3)

--- djangocms_navigation/models.py
+++ djangocms_navigation/models.py
@@ -4,9 +4,14 @@ from django.contrib.sites.models import Site
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from cms.models import CMSPlugin
+
 from treebeard.mp_tree import MP_Node
 
-from .constants import TARGETS
+from .constants import TARGETS, TEMPLATE_DEFAULT, get_templates
+
+
+__all__ = ["Menu", "MenuContent", "MenuItem", "NavigationPlugin"]
 
 
 class Menu(models.Model):
@@ -39,3 +44,19 @@ class MenuItem(MP_Node):
 
     def __str__(self):
         return self.title
+
+
+class NavigationPlugin(CMSPlugin):
+    template = models.CharField(
+        verbose_name=_("Template"),
+        choices=get_templates(),
+        default=TEMPLATE_DEFAULT,
+        max_length=255,
+    )
+
+    class Meta:
+        verbose_name = _("navigation plugin model")
+        verbose_name_plural = _("navigation plugin models")
+
+    def __str__(self):
+        return self.get_template_display()
