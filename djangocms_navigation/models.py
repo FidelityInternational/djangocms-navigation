19e565cd511a7529d75e218eb0882d27750b73e0
Monika Sulik <kontakt@monikasulik.net>
2019-01-07 10:43:43 +0000

    Add PROTECT to foreign key

--- djangocms_navigation/models.py
+++ djangocms_navigation/models.py
@@ -28,7 +28,8 @@ class Menu(models.Model):
 
 class MenuContent(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
-    root = models.ForeignKey('djangocms_navigation.MenuItem')
+    root = models.ForeignKey(
+        'djangocms_navigation.MenuItem', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
