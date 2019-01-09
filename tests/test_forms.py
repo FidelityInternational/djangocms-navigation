b5f2cec8b3ca8bb33472e0948da4ed42917756cf
Monika Sulik <kontakt@monikasulik.net>
2019-01-09 11:17:31 +0000

    More form tests

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -7,18 +7,21 @@ from djangocms_navigation.test_utils import factories
 class MenuContentFormTestCase(CMSTestCase):
     # Todo: add more test while change object field to autocomplete
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.menu_root = factories.RootMenuItemFactory()
+
     def test_valid_if_adding_child_of_existing_root_node(self):
-        item = factories.RootMenuItemFactory()
         data = {
             "title": "My new Title",
             "content_type": 1,
             "object_id": 1,
-            "_ref_node_id": item.id,
+            "_ref_node_id": self.menu_root.id,
             "numchild": 1,
             "link_target": "_self",
             "_position": "first-child",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -28,7 +31,7 @@ class MenuContentFormTestCase(CMSTestCase):
         self.assertTrue(is_valid)
 
     def test_valid_if_adding_child_of_existing_child_node(self):
-        item = factories.ChildMenuItemFactory()
+        item = factories.ChildMenuItemFactory(parent=self.menu_root)
         data = {
             "title": "My new Title",
             "content_type": 1,
@@ -38,7 +41,7 @@ class MenuContentFormTestCase(CMSTestCase):
             "link_target": "_self",
             "_position": "first-child",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -48,7 +51,7 @@ class MenuContentFormTestCase(CMSTestCase):
         self.assertTrue(is_valid)
 
     def test_valid_if_adding_right_sibling_of_existing_child_node(self):
-        item = factories.ChildMenuItemFactory()
+        item = factories.ChildMenuItemFactory(parent=self.menu_root)
         data = {
             "title": "My new Title",
             "content_type": 1,
@@ -58,7 +61,7 @@ class MenuContentFormTestCase(CMSTestCase):
             "link_target": "_self",
             "_position": "right",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -68,7 +71,7 @@ class MenuContentFormTestCase(CMSTestCase):
         self.assertTrue(is_valid)
 
     def test_valid_if_adding_left_sibling_of_existing_child_node(self):
-        item = factories.ChildMenuItemFactory()
+        item = factories.ChildMenuItemFactory(parent=self.menu_root)
         data = {
             "title": "My new Title",
             "content_type": 1,
@@ -78,7 +81,7 @@ class MenuContentFormTestCase(CMSTestCase):
             "link_target": "_self",
             "_position": "left",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -97,7 +100,7 @@ class MenuContentFormTestCase(CMSTestCase):
             "link_target": "_self",
             "_position": "first-child",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -123,7 +126,7 @@ class MenuContentFormTestCase(CMSTestCase):
             "link_target": "_self",
             "_position": "left",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -149,7 +152,7 @@ class MenuContentFormTestCase(CMSTestCase):
             "link_target": "_self",
             "_position": "right",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -166,17 +169,16 @@ class MenuContentFormTestCase(CMSTestCase):
         )
 
     def test_invalid_if_trying_to_add_right_sibling_of_existing_root_node(self):
-        item = factories.RootMenuItemFactory()
         data = {
             "title": "My new Title",
             "content_type": 1,
             "object_id": 1,
-            "_ref_node_id": item.id,
+            "_ref_node_id": self.menu_root.id,
             "numchild": 1,
             "link_target": "_self",
             "_position": "right",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -193,17 +195,16 @@ class MenuContentFormTestCase(CMSTestCase):
         )
 
     def test_invalid_if_trying_to_add_left_sibling_of_existing_root_node(self):
-        item = factories.RootMenuItemFactory()
         data = {
             "title": "My new Title",
             "content_type": 1,
             "object_id": 1,
-            "_ref_node_id": item.id,
+            "_ref_node_id": self.menu_root.id,
             "numchild": 1,
             "link_target": "_self",
             "_position": "left",
         }
-        form = MenuItemForm(data)
+        form = MenuItemForm(menu_root=self.menu_root, data=data)
         # Hack the form so there's a valid choice for object_id
         # TODO: This will need to be modified when autocomplete is added
         form.fields['object_id'].choices = [(1, 'Object')]
@@ -218,3 +219,28 @@ class MenuContentFormTestCase(CMSTestCase):
             # there's some other default then that's fine too.
             ['You cannot add a sibling for this menu item']
         )
+
+    def test_only_display_node_tree_of_current_root(self):
+        child = factories.ChildMenuItemFactory(parent=self.menu_root)
+        root2 = factories.RootMenuItemFactory()
+        child_of_root2 = factories.ChildMenuItemFactory(parent=root2)
+        form = MenuItemForm(menu_root=self.menu_root)
+
+        menu_item_ids = [
+            choice[0] for choice in form.declared_fields['_ref_node_id'].choices]
+
+        # The menu items that should be in choices are indeed there
+        self.assertIn(self.menu_root.pk, menu_item_ids)
+        self.assertIn(child.pk, menu_item_ids)
+        # Those from other root nodes are not
+        self.assertNotIn(root2.pk, menu_item_ids)
+        self.assertNotIn(child_of_root2.pk, menu_item_ids)
+        # And the general count is correct
+        # TODO: If --root-- is removed then this should be 4 not 5
+        self.assertEqual(len(form.declared_fields['_ref_node_id'].choices), 5)
+
+    def test_do_not_display_root_option_in_relative_field(self):
+        form = MenuItemForm(menu_root=self.menu_root)
+        menu_item_ids = [
+            choice[0] for choice in form.declared_fields['_ref_node_id'].choices]
+        self.assertNotIn(0, menu_item_ids)
