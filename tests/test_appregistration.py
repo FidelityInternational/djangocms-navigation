81b2f23dbecd5db4c9a8d4bf153423c454ae6bf4
Monika Sulik <kontakt@monikasulik.net>
2019-01-17 11:59:49 +0000

    Tests for navigation settings

--- tests/test_appregistration.py
+++ tests/test_appregistration.py
@@ -1,5 +1,5 @@
 import imp
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 from django.apps import apps
 from django.core.exceptions import ImproperlyConfigured
@@ -7,9 +7,10 @@ from django.test import override_settings
 
 from cms import app_registration
 from cms.models import Page
-from cms.utils.setup import setup_cms_apps
+from cms.utils.setup import setup_cms_apps, configure_cms_apps
 
 from djangocms_navigation import cms_config
+from djangocms_navigation.models import MenuContent
 from djangocms_navigation.test_utils.app_1.models import TestModel1, TestModel2
 from djangocms_navigation.test_utils.app_2.models import TestModel3, TestModel4
 from djangocms_navigation.utils import supported_models
@@ -20,34 +21,34 @@ from .utils import TestCase
 class AppRegistrationTestCase(TestCase):
     def test_missing_cms_config(self):
         extensions = cms_config.NavigationCMSExtension()
-        cms_config = Mock(
+        config = Mock(
             djangocms_navigation_enabled=True, app_config=Mock(label="blah_cms_config")
         )
 
         with self.assertRaises(ImproperlyConfigured):
-            extensions.configure_app(cms_config)
+            extensions.configure_app(config)
 
     def test_invalid_cms_config_parameter(self):
         extensions = cms_config.NavigationCMSExtension()
-        cms_config = Mock(
+        config = Mock(
             djangocms_navigation_enabled=True,
             navigation_models=23234,
             app_config=Mock(label="blah_cms_config"),
         )
 
         with self.assertRaises(ImproperlyConfigured):
-            extensions.configure_app(cms_config)
+            extensions.configure_app(config)
 
     def test_valid_cms_config_parameter(self):
         extensions = cms_config.NavigationCMSExtension()
-        cms_config = Mock(
+        config = Mock(
             djangocms_navigation_enabled=True,
             navigation_models=[TestModel1, TestModel2, TestModel3, TestModel4],
             app_config=Mock(label="blah_cms_config"),
         )
 
         with self.assertNotRaises(ImproperlyConfigured):
-            extensions.configure_app(cms_config)
+            extensions.configure_app(config)
             register_model = []
             for model in extensions.navigation_apps_models:
                 register_model.append(model)
@@ -71,26 +72,106 @@ class NavigationIntegrationTestCase(TestCase):
         setup_cms_apps()
         registered_models = supported_models()
 
-        expected_models = [TestModel1, TestModel2, TestModel3, TestModel4, Page]
-        self.assertCountEqual(registered_models, expected_models)
+        expected_models = [Page, TestModel1, TestModel2, TestModel3, TestModel4]
+        self.assertListEqual(registered_models, expected_models)
 
 
-class SettingsTestCase(TestCase):
+class NavigationSettingTestCase(TestCase):
     def setUp(self):
-        self.extension = cms_config.NavigationCMSExtension()
-        self.extension.navigation_apps_models = []
         self.app = apps.get_app_config('djangocms_navigation')
+        # Empty the list of registered models so it gets populated
+        # from scratch in tests
+        self.app.cms_extension.navigation_apps_models = []
+
+    def tearDown(self):
+        # Populate everything again so our setting changes do not effect
+        # any other tests
+        imp.reload(cms_config)
+        self.app.cms_extension.navigation_apps_models = []
+        self.app.cms_config = cms_config.NavigationCMSAppConfig(self.app)
+        configure_cms_apps([self.app])
+
+    def test_cms_models_added_to_navigation_by_default(self):
+        imp.reload(cms_config)  # Reload so setting gets checked again
+        # The app should have a cms config with the overridden setting
+        self.app.cms_config = cms_config.NavigationCMSAppConfig(self.app)
+
+        configure_cms_apps([self.app])
+
+        self.assertIn(Page, self.app.cms_extension.navigation_apps_models)
 
     @override_settings(NAVIGATION_CMS_MODELS_ENABLED=True)
     def test_cms_models_added_to_navigation_if_enabled(self):
-        imp.reload(cms_config)
-        nav_app_config = cms_config.NavigationCMSAppConfig(self.app)
-        self.extension.configure_app(nav_app_config)
-        self.assertIn(Page, self.extension.navigation_apps_models)
+        imp.reload(cms_config)  # Reload so setting gets checked again
+        # The app should have a cms config with the overridden setting
+        self.app.cms_config = cms_config.NavigationCMSAppConfig(self.app)
+
+        configure_cms_apps([self.app])
+
+        self.assertIn(Page, self.app.cms_extension.navigation_apps_models)
 
     @override_settings(NAVIGATION_CMS_MODELS_ENABLED=False)
     def test_cms_models_not_added_to_navigation_if_disabled(self):
+        imp.reload(cms_config)  # Reload so setting gets checked again
+        # The app should have a cms config with the overridden setting
+        self.app.cms_config = cms_config.NavigationCMSAppConfig(self.app)
+
+        configure_cms_apps([self.app])
+
+        self.assertNotIn(Page, self.app.cms_extension.navigation_apps_models)
+
+
+class VersioningSettingTestCase(TestCase):
+    def setUp(self):
+        self.versioning_app = apps.get_app_config('djangocms_versioning')
+        # Empty the list of registered models so it gets populated
+        # from scratch in tests
+        self.versioning_app.cms_extension.versionables = []
+
+    def tearDown(self):
+        """Populate everything again so our setting changes do not
+        effect any other tests"""
+        # Set the defaults for the navigation app config again
         imp.reload(cms_config)
-        nav_app_config = cms_config.NavigationCMSAppConfig(self.app)
-        self.extension.configure_app(nav_app_config)
-        self.assertNotIn(Page, self.extension.navigation_apps_models)
+        navigation_app = apps.get_app_config('djangocms_navigation')
+        navigation_app.cms_config = cms_config.NavigationCMSAppConfig(navigation_app)
+        # Reset the versioning app
+        self.versioning_app.cms_extension.versionables = []
+        configure_cms_apps([self.versioning_app])
+
+    def test_navigation_is_versioned_by_default(self):
+        imp.reload(cms_config)  # Reload so setting gets checked again
+        # The app should have a cms config with the overridden setting
+        navigation_app = apps.get_app_config('djangocms_navigation')
+        navigation_app.cms_config = cms_config.NavigationCMSAppConfig(navigation_app)
+
+        with patch.object(app_registration, 'get_cms_config_apps', return_value=[navigation_app]):
+            configure_cms_apps([self.versioning_app])
+
+        self.assertEqual(len(self.versioning_app.cms_extension.versionables), 1)
+        self.assertEqual(self.versioning_app.cms_extension.versionables[0].content_model, MenuContent)
+
+    @override_settings(NAVIGATION_VERSIONING_ENABLED=True)
+    def test_navigation_is_versioned_if_versioning_setting_enabled(self):
+        imp.reload(cms_config)  # Reload so setting gets checked again
+        # The app should have a cms config with the overridden setting
+        navigation_app = apps.get_app_config('djangocms_navigation')
+        navigation_app.cms_config = cms_config.NavigationCMSAppConfig(navigation_app)
+
+        with patch.object(app_registration, 'get_cms_config_apps', return_value=[navigation_app]):
+            configure_cms_apps([self.versioning_app])
+
+        self.assertEqual(len(self.versioning_app.cms_extension.versionables), 1)
+        self.assertEqual(self.versioning_app.cms_extension.versionables[0].content_model, MenuContent)
+
+    @override_settings(NAVIGATION_VERSIONING_ENABLED=False)
+    def test_navigation_is_versioned_if_versioning_setting_disabled(self):
+        imp.reload(cms_config)  # Reload so setting gets checked again
+        # The app should have a cms config with the overridden setting
+        navigation_app = apps.get_app_config('djangocms_navigation')
+        navigation_app.cms_config = cms_config.NavigationCMSAppConfig(navigation_app)
+
+        with patch.object(app_registration, 'get_cms_config_apps', return_value=[navigation_app]):
+            configure_cms_apps([self.versioning_app])
+
+        self.assertEqual(len(self.versioning_app.cms_extension.versionables), 0)
