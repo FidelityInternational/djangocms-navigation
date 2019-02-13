ea6b4e49eb866807d821cb5a4fe45cbc01948f94
Rizwan Mansuri <Rizwan@webbyfox.com>
2019-02-13 08:17:36 +0000

    Refactor and rename

--- tests/test_cms_menus.py
+++ tests/test_cms_menus.py
@@ -73,21 +73,21 @@ class CMSMenuTestCase(CMSTestCase):
             attr={"link_target": child2.link_target},
         )
 
-    def test_get_roots_with_multiple_menucontent(self):
+    def test_get_roots_with_multiple_menucontents(self):
+        """test to check get_roots while creating two menu contents"""
         factories.MenuContentFactory()
         factories.MenuContentFactory()
         roots = self.menu.get_roots(self.request)
         self.assertEqual(roots.count(), 2)
 
 
-class CMSMenuWithVersioningTestCase(CMSTestCase):
+class CMSMenuVersioningTestCase(CMSTestCase):
     def setUp(self):
         self.menu = CMSMenu(None)
         self.request = RequestFactory().get("/")
         self.user = self.get_superuser()
-        self.client.force_login(self.user)
 
-        # settting up menu item with versioning data
+        # setting up menu content with versions
         menuitem_1 = factories.RootMenuItemFactory()
         menuitem_1_v1 = factories.MenuVersionFactory(content__root=menuitem_1)
         menuitem_1_v1.archive(self.user)
@@ -95,13 +95,17 @@ class CMSMenuWithVersioningTestCase(CMSTestCase):
         menuitem_2 = factories.RootMenuItemFactory()
         factories.MenuVersionFactory(content__root=menuitem_2)
 
-    def test_get_roots_multiple_menucontent_versions_with_versioning(self):
+    def test_get_roots_with_versioning_enabled(self):
+        """This test to check versioning would group all the versions
+        of menu content and return latest of all distinct menu content
+        """
         roots = self.menu.get_roots(self.request)
-        # it should return two menucontent for versioning enable
         self.assertEqual(roots.count(), 2)
 
     @disable_versioning_for_navigation()
-    def test_get_roots_multiple_versions_without_versioning(self):
+    def test_get_roots_with_versioning_disabled(self):
+        """This test will check while versioning disabled it should check
+        against all menu content created
+        """
         roots = self.menu.get_roots(self.request)
-        # it should return three menucontent rather two for versioning disable
         self.assertEqual(roots.count(), 3)
