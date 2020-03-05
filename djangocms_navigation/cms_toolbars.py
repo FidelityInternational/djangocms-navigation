bc93cb996f3694ff093c004d45df4910a105b1f6
Andrew Aikman <andrew.aikman@fil.com>
2020-03-05 14:52:41 +0000

    Clean the cms toolbar and use better named vars

--- djangocms_navigation/cms_toolbars.py
+++ djangocms_navigation/cms_toolbars.py
@@ -8,15 +8,17 @@ from .models import MenuContent
 
 
 class NavigationToolbar(PlaceholderToolbar):
-    menu_model = MenuContent
+    menu_content_model = MenuContent
 
     def _add_navigation_menu(self):
         if not self.request.user.has_perm("{}.change_menucontent".format(
-            self.menu_model._meta.app_label)
-        ):
+            self.menu_content_model._meta.app_label
+        )):
             return
         admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER)
-        url = reverse(f"admin:{self.menu_model._meta.app_label}_menucontent_changelist")
+        url = reverse("admin:{}_menucontent_changelist".format(
+            self.menu_content_model._meta.app_label)
+        )
         admin_menu.add_sideframe_item(_("Navigation"), url=url, position=4)
 
     def post_template_populate(self):
