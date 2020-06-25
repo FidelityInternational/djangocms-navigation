b6245f627f2577f02f109aec807e002a6d57fcf4
Adam Murray <adam@Adams-MacBook-Pro-2.local>
2020-06-25 14:05:12 +0100

    Fixed MenuContent Issues

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -45,8 +45,6 @@ class MenuItemChangelistTestCase(CMSTestCase):
         request.menu_content_id = menu_content.pk
         request.user = self.get_superuser()
         model_admin = self.site._registry[MenuItem]
-        import pdb
-        pdb.set_trace()
         admin_field = "title"
 
         args = [
