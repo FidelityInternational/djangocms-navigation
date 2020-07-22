174148060c2c50ae1ba77b1a724e9047ae0b31c0
narender81 <narender.bongoni@promatrixsolutions.co.uk>
2020-07-22 16:56:50 +0100

    revert back no of queries to 5

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -156,9 +156,9 @@ class TestNavigationPerformance(CMSTestCase):
         factories.ChildMenuItemFactory(parent=child3)
 
         page_url = page_content.page.get_absolute_url()
-        with self.assertNumQueries(7):
+        with self.assertNumQueries(5):
             self.client.get(page_url)
 
         child3.soft_root = True
-        with self.assertNumQueries(7):
+        with self.assertNumQueries(5):
             self.client.get(page_url)
