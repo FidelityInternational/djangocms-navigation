1515b35072d3d59e1e9c16cd8123e01c3259372f
Andrew Aikman <andrew.aikman@eliga.services>
2022-03-21 16:27:49 +0000

    Fix isort error

--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, re_path, url
+from django.conf.urls import include, re_path
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
