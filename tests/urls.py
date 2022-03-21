4440f31b7b25aea42cf0a7ab5eb6240bfa8bd8c1
Andrew Aikman <andrew.aikman@eliga.services>
2022-03-21 16:26:44 +0000

    Cleaned out the logic for app_helper

--- tests/urls.py
+++ tests/urls.py
@@ -6,27 +6,18 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.i18n import JavaScriptCatalog
 from django.views.static import serve
 
-from app_helper.utils import load_from_file
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT, "show_indexes": True}),  # NOQA
-    url(r"^jsi18n/(?P<packages>\S+?)/$", JavaScriptCatalog.as_view()),  # NOQA
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT, "show_indexes": True}),  # NOQA
+    re_path(r"^jsi18n/(?P<packages>\S+?)/$", JavaScriptCatalog.as_view()),  # NOQA
 ]
 i18n_urls = [
-    url(r"^admin/", admin.site.urls),
-    re_path('', include('djangocms_references.urls')),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^", include("cms.urls"))
+    re_path(r"^", include('djangocms_references.urls')),
 ]
-
-try:
-    load_from_file("%s.urls" % settings.BASE_APPLICATION)
-    i18n_urls.append(url(r"^%s/" % settings.BASE_APPLICATION, include("%s.urls" % settings.BASE_APPLICATION)))  # NOQA
-except OSError:  # pragma: no cover
-    pass
-
-if settings.USE_CMS:
-    i18n_urls.append(url(r"^", include("cms.urls")))  # NOQA
 
 urlpatterns += i18n_patterns(*i18n_urls)
 urlpatterns += staticfiles_urlpatterns()
