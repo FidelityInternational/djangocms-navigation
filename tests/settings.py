c5592c09f1bb13bf27639ec869def16699403661
Vipul Narang <vipul.narang95@gmail.com>
2024-03-13 17:20:17 +0530

    corrected test settings

--- tests/settings.py
+++ tests/settings.py
@@ -41,9 +41,9 @@ HELPER_SETTINGS = {
 
 def run():
     from app_helper import runner
+    runner.cms("djangocms_navigation", extra_args=[])
     from djangocms_navigation.compat import DJANGO_4_2
     from cms.test_utils.testcases import CMSTestCase
-    runner.cms("djangocms_navigation", extra_args=[])
     if not DJANGO_4_2:
         CMSTestCase.assertQuerySetEqual = CMSTestCase.assertQuerysetEqual
 
